225ef951567e7cc29feb0251bae2d4d52244d06f
derrickwu1984 <derrickwu@sina.com>
2019-01-29 17:58:16 +0800

    login success

--- mobile/spiders/cbss.py
+++ mobile/spiders/cbss.py
@@ -21,40 +21,45 @@ class CbssSpider(scrapy.Spider):
     # 登陆后的链接
     initmy_url = "https://sd.cbss.10010.com/essframe"
     # initmy_url = "https://sd.cbss.10010.com/abcde"
-    driver_path="Z:\BaiduNetdiskDownload\IEDriverServer.exe"
+    # driver_path="Z:\BaiduNetdiskDownload\IEDriverServer.exe"
+    driver_path = "Z:/tools/IEDriverServer.exe"
     captha_image_url="https://hq.cbss.10010.com/image?mode=validate&width=60&height=20"
-    userName=""
-    passWd=""
+    userName="sdsc-xingyy7"
+    passWd="BySh@2019"
 
     def start_requests(self):
         yield scrapy.Request(self.login_url, callback=self.login)
 
     def login(self, response):
         driver = webdriver.Ie(self.driver_path)
-        res=driver.get(self.login_url)
+        driver.get(self.login_url)
         time.sleep(2)
         # driver.maximize_window()
         driver.find_element_by_id("STAFF_ID").send_keys(self.userName)
         driver.find_element_by_id("LOGIN_PASSWORD").send_keys(self.passWd)
         Select(driver.find_element_by_name("LOGIN_PROVINCE_CODE")).select_by_value("17")
-        # captcha_url = driver.find_element_by_xpath("//div[@id='VerifyPart2']/img/@src")
-        # logging.debug((captcha_url))
-        captcha_image = requests.get(self.captha_image_url,verify=False)
-        # 打开验证码图片
-        img = Image.open(BytesIO(captcha_image.content))
-        # 显示验证码
-        img.show()
-        # 输入识别出的验证码
-        captcha_text = input(u'请输入验证码:')
-        driver.find_element_by_id("VERIFY_CODE").send_keys(captcha_text)
-        driver.find_element_by_xpath("//div[@class='submit clear']/input[1]").click()
-        # all_options = select.find_elements_by_tag_name("option")
         logging.debug("------start------")
         WebDriverWait(driver, 1000).until(EC.url_to_be(self.initmy_url))
         logging.debug("恭喜您，您已登录成功了！")
-        yield scrapy.Request(url=self.initmy_url, callback=self.parse)
+        res =driver.page_source
+        logging.debug(res)
+        cookies_dict = {}
+        cookies = driver.get_cookies()
+        for cookie in cookies:
+            cookies_dict[cookie['name']] = cookie['value']
+        with open('cookies.txt', 'w+') as f:
+            json.dump(cookies_dict, f)
+        with open('cookies.txt', 'r') as f:
+            cookie_out = json.load(f)
+            logging.debug(cookie_out)
+        headers = {
+            'referer': 'https://cbss.10010.com/essframe',
+            'cookie': cookie_out
+        }
+        # yield scrapy.Request(url=self.initmy_url, callback=self.parse,headers=headers)
     def parse(self, response):
         logging.debug("=====start parse========")
-        ele=response.xpath("//div[@class='nav e_clearfix'])").extract()
-        logging.debug(ele)
+        # html = etree.HTML(response.text)
+        # ele=response.xpath("//div[@class='nav e_clearfix'])").extract()
+        # logging.debug(response.text)
         pass
