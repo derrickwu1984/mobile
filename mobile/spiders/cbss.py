4ac269e0f46aa1cc971cd11e10abdaade39076c4
derrickwu1984 <derrickwu@sina.com>
2019-02-13 17:35:05 +0800

    requests

--- mobile/spiders/cbss.py
+++ mobile/spiders/cbss.py
@@ -32,81 +32,49 @@ class CbssSpider(scrapy.Spider):
     captha_image = "Z:\\Users\\wumingxing\\Desktop\\captha.png"
     userName=""
     passWd=""
-    js_method="clickMenuItem(this);openmenu('/acctmanm?service=page/amarchquery.queryaccountbill.QueryAccountBill&listener=myInitialize&RIGHT_CODE=BIL651P&LOGIN_RANDOM_CODE=1548903104099624120380&LOGIN_CHECK_CODE=201901311746371775&LOGIN_PROVINCE_CODE=17&IPASS_LOGIN=null&gray_staff_id=sdsc-xingyy7&gray_depart_id=17b5q7m&gray_province_code=17&gray_eparchy_code=0531');"
     js_exec="var but_click=document.getElementsByClassName('submit')[0].children[0].onclick"
     # js_exec="var but_click=document.querySelector('.button')"
+    reqeust_url="https://sd.cbss.10010.com/acctmanm?service=page/amarchquery.queryuserbill.QueryUserBillCBss&listener=myInitialize&RIGHT_CODE=ASMUSERTABQRY&LOGIN_RANDOM_CODE=15500242660371590672351&LOGIN_CHECK_CODE=201902131747274690&LOGIN_PROVINCE_CODE=17&IPASS_LOGIN=null&gray_staff_id=sdsc-xingyy7&gray_depart_id=17b5q7m&gray_province_code=17&gray_eparchy_code=0531&staffId=sdsc-xingyy7&departId=17b5q7m&subSysCode=CBS&eparchyCode=0531"
     def start_requests(self):
         yield scrapy.Request(self.login_url, callback=self.login)
-
     def login(self, response):
         driver = webdriver.Ie(self.driver_path)
         driver.get(self.login_url)
-        time.sleep(2)
-        builder=ActionChains(driver)
-        # builder.send_keys(Keys.F12).perform()
-        time.sleep(2)
-        driver.refresh()
+        # time.sleep(2)
+        # builder=ActionChains(driver)
+        # time.sleep(2)
+        # driver.refresh()
         time.sleep(5)
         driver.find_element_by_id("STAFF_ID").send_keys(self.userName)
         driver.find_element_by_id("LOGIN_PASSWORD").send_keys(self.passWd)
         Select(driver.find_element_by_name("LOGIN_PROVINCE_CODE")).select_by_value("17")
-        time.sleep(2)
-        # driver.save_screenshot(self.captha_image_path)
-        # locate_image=driver.find_element_by_id("captureImage").location
-        # size_image=driver.find_element_by_id("captureImage").size
-        # left=locate_image['x']
-        # top=locate_image['y']
-        # right=locate_image['x']+size_image['width']
-        # bottom = locate_image['y']+size_image['height']
-
-        # rangle=(int (left),int(top),int(right),int(bottom))
-        # i = Image.open(self.captha_image_path)
-        # segment  = i.crop(rangle)
-        # segment.save(self.captha_image)
-        # i = Image.open(self.captha_image)
-        # i.show()
-        captha_input=input(u"请输入验证码:")
-        VERIFY_CODE_ELE = driver.find_element_by_id("VERIFY_CODE")
-        VERIFY_CODE_ELE.send_keys(captha_input)
-        time.sleep(2)
+        # time.sleep(2)
+        # captha_input=input(u"请输入验证码:")
+        # VERIFY_CODE_ELE = driver.find_element_by_id("VERIFY_CODE")
+        # VERIFY_CODE_ELE.send_keys(captha_input)
+        # time.sleep(2)
         logging.debug("------start------")
         WebDriverWait(driver, 30).until(EC.url_to_be(self.initmy_url))
         logging.debug("恭喜您，您已登录成功了！")
         # 如果没有使用此行代码，则无法找到页面frame中的任何页面元素
         driver.switch_to.frame("navframe")
-        time.sleep(20)
+        time.sleep(10)
         # logging.warning("=========first==========")
         # logging.warning(driver.page_source)
         js_query_acct="var query_acct=document.getElementById('SECOND_MENU_LINK_BIL6500').onclick()"
-        driver.execute_script(js_query_acct)
-        logging.warning("=========second==========")
-        logging.warning(driver.page_source)
+        # driver.execute_script(js_query_acct)
+        time.sleep(3)
+        # logging.warning("=========second==========")
+        # logging.warning(driver.page_source)
         js_query_bill = "var query_acct=document.getElementById('BIL651P').onclick()"
         driver.execute_script(js_query_acct)
         logging.warning("=========third==========")
-        logging.warning(driver.page_source)
-        # assert first_menu.is_displayed()
-        # first_menu.click()
-        # logging.debug("==========after click first_menu response==============")
-        # logging.debug(driver.page_source)
-        # time.sleep(30)
-        # second_menu=driver.find_element_by_id("SECOND_MENU_LINK_BIL6500")
-        # logging.warning("==========second_menu===========")
-        # logging.warning(second_menu)
-        # for i in range(3):
-        #     try:
-        #         assert second_menu.is_displayed()
-        #     except :
-        #         logging.error("no Element displayed")
-        #     else:
-        #         second_menu.click()
-        # logging.debug("==========after click second_menu response==============")
-        # logging.debug(driver.page_source)
-        # driver.execute_script(self.js_method)
-        # logging.warning(driver.execute_script(self.js_method))
-        # a_tag=driver.find_element_by_id("BIL651P")
-        # logging.warning(a_tag)
-        # a_tag.click()
+        # CSM1001=driver.find_element_by_id("CSM1001")
+        CSM1001_js="document.getElementById('CSM1001').getAttribute('onclick')"
+        CSM1001=driver.execute_script(CSM1001_js)
+        requests.adapters.DEFAULT_RETRIES = 5
+        s = requests.session()
+        # s.keep_alive = False  # 关闭多余连接
         cookies_dict = {}
         cookies = driver.get_cookies()
         for cookie in cookies:
@@ -115,12 +83,15 @@ class CbssSpider(scrapy.Spider):
             json.dump(cookies_dict, f)
         with open('cookies.txt', 'r') as f:
             cookie_out = json.load(f)
-            logging.debug(cookie_out)
         headers = {
-            'referer': 'https://cbss.10010.com/essframe',
-            'cookie': cookie_out
+            'referer': 'https://sd.cbss.10010.com/essframe?service=page/component.Navigation&listener=init&needNotify=true&staffId=sdsc-xingyy7&departId=17b5q7m&subSysCode=CBS&eparchyCode=0531',
+            'Host':'sd.cbss.10010.com'
+            # 'cookie': dict(cookie_out)
         }
-        # yield scrapy.Request(url=self.initmy_url, callback=self.parse,headers=headers)
+        res=s.get(self.reqeust_url,headers=headers,cookies=cookie_out,verify= False)
+        time.sleep(5)
+        logging.warning(res.content.decode('gbk'))
+        # res.content
     def parse(self, response):
         logging.debug("=====start parse========")
         pass
