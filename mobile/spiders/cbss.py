6c95978602a06e7d63fd6aca296d5d675dd2bedc
derrickwu1984 <derrickwu@sina.com>
2019-02-14 21:58:55 +0800

    fuck cbss

--- mobile/spiders/cbss.py
+++ mobile/spiders/cbss.py
@@ -26,9 +26,8 @@ class CbssSpider(scrapy.Spider):
     login_url = "https://cbss.10010.com/essframe"
     # 登陆后的链接
     initmy_url = "https://sd.cbss.10010.com/essframe"
-    # initmy_url = "https://sd.cbss.10010.com/abcde"
-    # driver_path="D:/tools/IEDriverServer.exe"
-    driver_path = "Z:/tools/IEDriverServer.exe"
+    driver_path="D:/tools/IEDriverServer.exe"
+    # driver_path = "Z:/tools/IEDriverServer.exe"
     captha_image_url="https://hq.cbss.10010.com/image?mode=validate&width=60&height=20"
     captha_image_path="Z:\\Users\\wumingxing\\Desktop\\printscreen.png"
     captha_image = "Z:\\Users\\wumingxing\\Desktop\\captha.png"
@@ -62,7 +61,6 @@ class CbssSpider(scrapy.Spider):
         driver.switch_to.frame("navframe")
         time.sleep(30)
         # logging.warning("=========first==========")
-        logging.warning(driver.page_source)
         js_query_acct="var query_acct=document.getElementById('SECOND_MENU_LINK_BIL6500').onclick()"
         driver.execute_script(js_query_acct)
         time.sleep(3)
@@ -71,13 +69,13 @@ class CbssSpider(scrapy.Spider):
         # js_query_bill = "var query_acct=document.getElementById('BIL651P').onclick()"
         # js_query_bill = "var query_acct=document.getElementById('BIL6531').onclick()"
         # driver.execute_script(js_query_bill)
-        logging.warning("=========third==========")
-        CSM1001=driver.find_element_by_id("CSM1001").get_attribute("onclick")
+        # logging.warning("=========third==========")
+        CSM1001 = driver.find_element_by_id("CSM1001").get_attribute("onclick")
         CSM1001_content=CSM1001.split('&')
         LOGIN_RANDOM_CODE =CSM1001_content[3]
         LOGIN_CHECK_CODE=CSM1001_content[4]
         reqeust_url = "https://sd.cbss.10010.com/acctmanm?service=page/amarchquery.queryuserbill.QueryUserBillCBss&listener=myInitialize&RIGHT_CODE=ASMUSERTABQRY&"+LOGIN_RANDOM_CODE+"&"+LOGIN_CHECK_CODE+"&LOGIN_PROVINCE_CODE=17&IPASS_LOGIN=null&gray_staff_id=sdsc-xingyy7&gray_depart_id=17b5q7m&gray_province_code=17&gray_eparchy_code=0531&staffId=sdsc-xingyy7&departId=17b5q7m&subSysCode=CBS&eparchyCode=0531"
-        logging.warning(reqeust_url)
+        # logging.warning(reqeust_url)
         requests.adapters.DEFAULT_RETRIES = 5
         s = requests.session()
         # s.keep_alive = False  # 关闭多余连接
@@ -97,15 +95,15 @@ class CbssSpider(scrapy.Spider):
         response_str=s.get(reqeust_url,headers=headers,cookies=cookie_out,verify= False)
         time.sleep(5)
         html=etree.HTML(response_str.content.decode('gbk'))
-        time.sleep(60)
-        logging.warning(response_str.content.decode('gbk'))
+        time.sleep(10)
+        # logging.warning(response_str.content.decode('gbk'))
         BSS_ACCTMANM_JSESSIONID=html.xpath('//form/@action')[0].split(";")[1]
         service=html.xpath('//input[@name="service"]/@value')[0]
         Form0=html.xpath('//input[@name="Form0"]/@value')[0]
-        logging.warning(html)
-        logging.warning(BSS_ACCTMANM_JSESSIONID)
-        logging.warning(service)
-        logging.warning(Form0)
+        # logging.warning(html)
+        # logging.warning(BSS_ACCTMANM_JSESSIONID)
+        # logging.warning(service)
+        # logging.warning(Form0)
         yy=datetime.datetime.now().year
         mm=datetime.datetime.now().month
         if (mm<10 and mm>1):
@@ -153,10 +151,19 @@ class CbssSpider(scrapy.Spider):
         }
         post_headers = {
             'referer':reqeust_url,
-            'Host':'sd.cbss.10010.com'
-            # 'cookie': dict(cookie_out)
+            'Host':'sd.cbss.10010.com',
+            # 'cookie': cookie_out
         }
-        data=requests.post(post_url,data=data,headers=post_headers,cookie=cookie_out,verify= False).content.decode("gbk")
+        BSS_ACCTMANM_JSESSIONID_array=BSS_ACCTMANM_JSESSIONID.split("=")
+        BSS_ACCTMANM_JSESSIONID_key=BSS_ACCTMANM_JSESSIONID_array[0]
+        BSS_ACCTMANM_JSESSIONID_value = BSS_ACCTMANM_JSESSIONID_array[1]
+        BSS_ACCTMANM_JSESSIONID_dict={BSS_ACCTMANM_JSESSIONID_key:BSS_ACCTMANM_JSESSIONID_value}
+        # logging.warn(BSS_ACCTMANM_JSESSIONID_dict)
+        # logging.warn("before",cookie_out)
+        cookie_out.update(BSS_ACCTMANM_JSESSIONID_dict)
+        # logging.warn("after",cookie_out)
+        # logging.warn(type(cookie_out))
+        data=requests.post(post_url,data=data,headers=post_headers,cookies=cookie_out,verify= False).content.decode("gbk")
         logging.warning(data)
     def parse(self, response):
         logging.debug("=====start parse========")
