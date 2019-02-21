a58d24a052fd308532ccbdc671836c6379246ce7
derrickwu1984 <derrickwu@sina.com>
2019-02-21 16:26:16 +0800

    alter table name

--- mobile/pipelines.py
+++ mobile/pipelines.py
@@ -4,7 +4,7 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
-import MySQLdb
+import MySQLdb,uuid
 
 class MobilePipeline(object):
     def __init__(self):
@@ -14,7 +14,7 @@ class MobilePipeline(object):
 
     def process_item(self, item, spider):
         insert_sql = """
-        insert into userinfo(crawldate,rangeno,phoneno,querymonth,
+        insert into cbss_mobile_userinfo_jinan(object_id,crawldate,rangeno,phoneno,querymonth,
                                   acctflag,paytype,debtfee,fixtype,
                                   payname,prodname,fee,openflag,
                                   custbrand,actualbal,custlocation,creditbal,
@@ -23,14 +23,16 @@ class MobilePipeline(object):
                                 %s,%s,%s,%s,
                                 %s,%s,%s,%s,
                                 %s,%s,%s,%s,
-                                %s,%s)
+                                %s,%s,%s)
             """
         if len(item) > 2:
-            self.curor.execute(insert_sql,
-                              (item["crawldate"], item["rangeno"], item["phoneno"], item["querymonth"]
-                                , item["acctflag"], item["paytype"], item["debtfee"], item["fixtype"]
-                                , item["payname"], item["prodname"], item["fee"], item["openflag"]
-                                , item["custbrand"], item["actualbal"], item["custlocation"], item["creditbal"]
-                                , item["totalfee"], item["actualfee"]))
-            self.conn.commit()
+                self.curor.execute(insert_sql,
+                                  (str(uuid.uuid1()),item["crawldate"], item["rangeno"], item["phoneno"], item["querymonth"]
+                                    , item["acctflag"], item["paytype"], item["debtfee"], item["fixtype"]
+                                    , item["payname"], item["prodname"], item["fee"], item["openflag"]
+                                    , item["custbrand"], item["actualbal"], item["custlocation"], item["creditbal"]
+                                    , item["totalfee"], item["actualfee"]))
+                self.conn.commit()
+
+
         return item
