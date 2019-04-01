06da5722edcd4f03045b648df11e9abdf81474e6
derrickwu1984 <derrickwu@sina.com>
2019-04-01 15:09:48 +0800

    add sys.argv[1]

--- mobile/main.py
+++ mobile/main.py
@@ -1,14 +1,10 @@
 from scrapy.cmdline import execute
-import  logging,_thread
+import  logging,sys,re
 
-# def mutil_process(thread_name,rangeNo):
-#     print("%s:%s" % (thread_name, rangeNo))
-execute(["scrapy", "crawl", "cbss", "-a", "rangeNo=1860008", "-a", "startNo=0000", "-a", "endNo=10000"])
-
-# try:
-#     _thread.start_new_thread(mutil_process,("thread-1","1300017",))
-    # _thread.start_new_thread(mutil_process,("thread-2","1310531",))
-# except:
-#     print ("error:unable to start thread")
-# while 1:
-#     pass
+phoneNo= sys.argv[1]
+if (re.findall("1[345678][01256]\d{4}",phoneNo) and len(phoneNo)==7):
+    # logging.warning(re.findall("1[345678][01256]\d{4}",phoneNo))
+    execute(["scrapy", "crawl", "cbss", "-a", "rangeNo="+sys.argv[1], "-a", "startNo=0000", "-a", "endNo=10000"])
+else:
+    logging.warning("请输入符合联通号段规则的7位电话号段！")
+    # exit()
