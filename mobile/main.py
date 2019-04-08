4d223920cf24b26da040d4bfe7e419a8fb939a42
derrickwu1984 <derrickwu@sina.com>
2019-04-08 15:00:40 +0800

    fuck main.py

--- mobile/main.py
+++ mobile/main.py
@@ -3,7 +3,8 @@ import  logging,sys,re
 
 # execute(["scrapy", "crawl", "cbss", "-a", "rangeNo=1860008" , "-a", "startNo=0000", "-a", "endNo=10000"])
 phoneNo= sys.argv[1]
-if (re.findall("1[345678][01256]\d{4}",phoneNo) and len(phoneNo)==7):
-    execute(["scrapy", "crawl", "cbss", "-a", "rangeNo="+sys.argv[1], "-a", "startNo=0000", "-a", "endNo=10000"])
-else:
-    logging.warning("请输入符合联通号段规则的7位电话号段！")
+# if (re.findall("1[345678][01256]\d{4}",phoneNo) and len(phoneNo)==7):
+execute(["scrapy", "crawl", "cbss", "-a", "rangeNo="+phoneNo, "-a", "startNo=0000", "-a", "endNo=10000"])
+# else:
+#     logging.warning("请输入符合联通号段规则的7位电话号段！")
+#
